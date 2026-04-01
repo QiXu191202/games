681a0fc1646d1df5a9ba0d5c5f227146b03bf2d2
Jason <q18297858664@163.com>
2026-04-01 20:59:56 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import heroImg from './assets/hero.png';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <section id='center'>
+        <canvas id="game" width="400" height="600"></canvas>
+      </section>
+    </>
+  );
+}
+
+export default App;
